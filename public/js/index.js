fa2150a793198e51b8044b297240c07d82819dce
erictseng611 <etseng@ucsd.edu>
2016-12-26 23:28:56 -0800

    animated scrolling

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+$(document.getElementById('scrollToAbout')).click(function() {
+     $('html, body').animate({
+         scrollTop: $(document.getElementById('about')).offset().top - 43  }, 800);
+ });
+
+$(document.getElementById('scrollToProjects')).click(function() {
+     $('html, body').animate({
+         scrollTop: $(document.getElementById('projects')).offset().top - 43  }, 800);
+ });
+
+$(document.getElementById('scrollToContact')).click(function() {
+     $('html, body').animate({
+         scrollTop: $(document.getElementById('contact')).offset().top - 43  }, 800);
+ });
