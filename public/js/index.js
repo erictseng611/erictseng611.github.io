f2d3c4d8b3902071c3836507acd4060b3e7c2b4b
erictseng611 <etseng@ucsd.edu>
2016-12-27 23:47:32 -0800

    fiddling with navbar margins

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 $(document.getElementById('scrollToAbout')).click(function() {
      $('html, body').animate({
-         scrollTop: $(document.getElementById('about')).offset().top - 55  }, 800);
+         scrollTop: $(document.getElementById('about')).offset().top - 70  }, 800);
  });
 
 $(document.getElementById('scrollToProjects')).click(function() {
      $('html, body').animate({
-         scrollTop: $(document.getElementById('projects')).offset().top - 55  }, 800);
+         scrollTop: $(document.getElementById('projects')).offset().top - 70  }, 800);
  });
 
 $(document.getElementById('scrollToContact')).click(function() {
      $('html, body').animate({
-         scrollTop: $(document.getElementById('contact')).offset().top - 55  }, 800);
+         scrollTop: $(document.getElementById('contact')).offset().top - 70  }, 800);
  });
