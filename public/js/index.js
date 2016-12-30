c7f7e557ca250013670c07f26cbf711376e782c8
erictseng611 <etseng@ucsd.edu>
2016-12-30 12:20:12 -0800

    dynamic nav bar

--- public/js/index.js
+++ public/js/index.js
@@ -17,5 +17,10 @@ $(document.getElementById('scrollToContact')).click(function() {
 $('.nav a').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
+    $(this).closest('li').addClass('current').siblings().removeClass('current')
 });
 
+$('body').scrollspy({
+  target: '#nav_bar',
+  offset: 70
+});
