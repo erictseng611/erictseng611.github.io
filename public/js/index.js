3f7a49204db6fae37b2cb3e291e62652d00f5531
erictseng611 <etseng@ucsd.edu>
2016-12-29 21:28:13 -0800

    fixed nav bug

--- public/js/index.js
+++ public/js/index.js
@@ -19,10 +19,3 @@ $('.nav a').on('click', function(){
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
 
-
- <div class="container">
-      <h1>Hi! I'm Eric Tseng. I am an aspiring UI/UX Designer and front-end web developer. </h1>
-    </div>
-    <div class="container" id="aboutInfo">
-      <h4> I am currently a third year student at the University of California, San Diego majoring in Cognitive Science w/ a specialization in Human Computer Interaction and a minor in Computer Science. </h4>
-    </div>
