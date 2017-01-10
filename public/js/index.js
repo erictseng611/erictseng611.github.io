444aedfd20fe21224737847addc65370203ee517
erictseng611 <etseng@ucsd.edu>
2017-01-09 19:40:57 -0800

    changed contact to a photo and added resume

--- public/js/index.js
+++ public/js/index.js
@@ -7,34 +7,34 @@ $(window).on('load', function() { // makes sure the whole site is loaded
         })
 
 
-            $(document.getElementById('scrollToAbout')).click(function() {
-            	$('html, body').animate({
-            		scrollTop: $(document.getElementById('about')).offset().top - 66  }, 800);
-            });
+$(document.getElementById('scrollToAbout')).click(function() {
+	$('html, body').animate({
+		scrollTop: $(document.getElementById('about')).offset().top - 66  }, 800);
+});
 
-            $(document.getElementById('scrollToProjects')).click(function() {
-            	$('html, body').animate({
-            		scrollTop: $(document.getElementById('projects')).offset().top - 70  }, 800);
-            });
+$(document.getElementById('scrollToProjects')).click(function() {
+	$('html, body').animate({
+		scrollTop: $(document.getElementById('projects')).offset().top - 70  }, 800);
+});
 
-            $(document.getElementById('scrollToContact')).click(function() {
-            	$('html, body').animate({
-            		scrollTop: $(document.getElementById('contact')).offset().top - 70  }, 800);
-            });
+$(document.getElementById('scrollToContact')).click(function() {
+	$('html, body').animate({
+		scrollTop: $(document.getElementById('contact')).offset().top - 70  }, 800);
+});
 
-            $(document.getElementById('downArrow')).click(function() {
-            	$('html, body').animate({
-            		scrollTop: $(document.getElementById('about')).offset().top - 70  }, 800);
-            });
+$(document.getElementById('downArrow')).click(function() {
+	$('html, body').animate({
+		scrollTop: $(document.getElementById('about')).offset().top - 70  }, 800);
+});
 
 
-            $('.nav a').on('click', function(){
+$('.nav a').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
     $(this).closest('li').addClass('current').siblings().removeClass('current')
 });
 
-            $('body').scrollspy({
-            	target: '#nav_bar',
-            	offset: 200
-            });
+$('body').scrollspy({
+	target: '#nav_bar',
+	offset: 400
+});
