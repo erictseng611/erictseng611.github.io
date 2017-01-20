23ca9ebd7fc037603e411fe68125d26c516eae43
erictseng611 <etseng@ucsd.edu>
2017-01-20 00:10:46 -0800

    navbar shows on hover

--- public/js/index.js
+++ public/js/index.js
@@ -9,22 +9,22 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 
 $(document.getElementById('scrollToAbout')).click(function() {
 	$('html, body').animate({
-		scrollTop: $(document.getElementById('about')).offset().top - 66  }, 800);
+		scrollTop: $(document.getElementById('about')).offset().top }, 800);
 });
 
 $(document.getElementById('scrollToProjects')).click(function() {
 	$('html, body').animate({
-		scrollTop: $(document.getElementById('projects')).offset().top - 70  }, 800);
+		scrollTop: $(document.getElementById('projects')).offset().top   }, 800);
 });
 
 $(document.getElementById('scrollToContact')).click(function() {
 	$('html, body').animate({
-		scrollTop: $(document.getElementById('contact')).offset().top - 70  }, 800);
+		scrollTop: $(document.getElementById('contact')).offset().top  }, 800);
 });
 
 $(document.getElementById('downArrow')).click(function() {
 	$('html, body').animate({
-		scrollTop: $(document.getElementById('about')).offset().top - 70  }, 800);
+		scrollTop: $(document.getElementById('about')).offset().top  }, 800);
 });
 
 
@@ -39,59 +39,6 @@ $('body').scrollspy({
 	target: '#nav_bar',
 	offset: 400
 });
-
-
-
-// $(document).ready(function(){
-
-// /** ===========================================
-//     Hide / show the master navigation menu
-//     ============================================ */
-
-//   // console.log('Window Height is: ' + $(window).height());
-//   // console.log('Document Height is: ' + $(document).height());
-
-//   var previousScroll = 0;
-
-//   $(window).scroll(function(){
-
-//   	var currentScroll = $(this).scrollTop();
-
-//     /*
-//       If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
-//       */
-//       if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
-
-//         If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
-
-//         if (currentScroll > previousScroll){
-//         	window.setTimeout(hideNav, 100);
-//       /*
-//         Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
-//         */
-//     } else {
-//     	window.setTimeout(showNav, 100);
-//     }
-//       /* 
-//         Set the previous scroll value equal to the current scroll.
-//         */
-//         previousScroll = currentScroll;
-//     }
-
-// });
-
-//   function hideNav() {
-//   	$("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden");
-//   }
-//   function showNav() {
-//   	$("[data-nav-status='toggle']").removeClass("is-hidden").addClass("is-visible");
-//   }
-
-// });
-
-
-
-
 
 
 var didScroll;
@@ -134,5 +81,23 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
+
+
+
+$(function () {
+    $('#shownav').hover(function () {
+        $('#nav_bar').removeClass('is-hidden');
+    });
+});
+
+/* when navbar is hovered over it will override previous */
+
+$(function () {
+    $('.navbar').hover(function () {
+        $('#nav_bar').removeClass('is-hidden');
+    }, function () {
+        $('#nav_bar').addClass('is-hidden');
+    });
+});
 
 
